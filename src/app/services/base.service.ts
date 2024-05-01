a4cadea23bf1a645714d145343e124dd7ca9bbe7
Felipe Ferreira <felipvut@gmail.com>
2024-04-30 21:59:31 -0300

    Update base.service.ts

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,8 +1,6 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Enviroment } from '../../enviroment/enviroment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
 @Injectable({
   providedIn: 'root',
 })
